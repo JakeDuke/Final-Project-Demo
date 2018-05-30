141ebee5a330772c7a9c64581d8e49524e73d8e8
christian1006 <37366207+christian1006@users.noreply.github.com>
2018-05-30 12:30:06 +0200

    input form with script.

--- public/onlyinputform.vue
+++ public/onlyinputform.vue
@@ -36,12 +36,66 @@
 
     import Axios from 'axios'
     import Vue from 'vue'
+    export default
 
     new Vue ({
         el: 'app'
         render: h => h(App)
     })
 
+        data: function () {
+            return {
+   
+                artist: {
+                    key: undefined,
+                    name: undefined,
+                    location: undefined,
+                    price: undefined,
+                    style: undefined,
+                    dates: undefined,
+                    about: undefined
+                },
+
+            }
+    },
+    computed: {
+        axiosInstance: function () {
+            return axios.create({
+                baseURL: 'http://localhost:3001/api',
+            })
+        }
+    },
+    methods: {
+
+        newArtist: function () {
+            
+            this.axiosInstance.post('/create', {
+            newArtist: this.artist   
+            })
+                .then((responce) => {
+                    console.log(responce.data);
+                    this.resetForm();
+                    this.$parent.$options.methods.getAllArtist.call(this.$parent)
+                    
+                    
+                })
+                .catch((error) => {
+                    if(error) {
+                        console.log(error);
+                    }
+            })
+
+            
+        },
+
+
+        resetForm: function() {
+            Object.keys(this.artist).forEach( key => this.artist[key] = '');
+        }
+    },
+
+
+}
 </script>
 
 <!-- input form end -->
